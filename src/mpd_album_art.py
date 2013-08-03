b4922ad717fe6b725f9b526aa1fd7cdccd66f8c6
jameh <jamie.alban@gmail.com>
2013-08-03 14:10:27 -0400

    Change title of module.

--- src/mpd_album_art.py
+++ src/mpd_album_art.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# MPD Album Art Grabber
+# MPD Album Art
 # This program is free software: you can redistribute it and/or 
 # modify it under the terms of the GNU General Public License as published 
 # by the Free Software Foundation, either version 3 of the License, or
@@ -13,9 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-MPD Album Art Grabber
-=====================
-
 :copyright: 2013 Jamie Macdonald
 :license: GNU General Public License (GPL) version 3
 
